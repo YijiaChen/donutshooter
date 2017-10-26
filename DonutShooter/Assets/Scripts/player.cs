aa82b7b39299753ab88b8d33c5b587ece6111af7
Yijia the Potato <whateverchen@yahoo.com>
2017-10-26 14:38:34 -0400

    Player movement.

--- DonutShooter/Assets/Scripts/player.cs
+++ DonutShooter/Assets/Scripts/player.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
-
+    public float movespeedx;
+    public float movespeedy;
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>(); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0,movespeedy);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0, -movespeedy);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-movespeedx, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(movespeedx, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
 		
 	}
 }
